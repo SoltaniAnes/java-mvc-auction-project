8f2a687dd884d6f30ab17e7298a19a225cdee3fc
zaouij <janis.zaoui@grenoble-inp.org>
2024-04-30 09:59:53 +0200

    Classe Produit.java

--- Produit.java
+++ Produit.java
@@ -1,2 +1,30 @@
 public class Produit {
+
+    private String Nom;
+    private int Prix_revient;
+    private int Stock;
+
+    public String getNom() {
+        return Nom;
+    }
+
+    public void setNom(String nom) {
+        Nom = nom;
+    }
+
+    public void setPrix_revient(int prix_revient) {
+        Prix_revient = prix_revient;
+    }
+
+    public void setStock(int Stock) {
+        this.Stock = Stock;
+    }
+
+    public int getPrix_revient() {
+        return Prix_revient;
+    }
+
+    public int getStock() {
+        return stock;
+    }
 }
