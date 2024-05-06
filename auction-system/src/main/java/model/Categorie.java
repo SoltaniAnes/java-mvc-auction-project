cae5172b51b6093882a0714812ac56ffc99dd6aa
Lilian Steimer <lilian.steimer@grenoble-inp.org>
2024-05-06 09:29:22 +0200

    Added Categorie class

---
+++ auction-system/src/main/java/model/Categorie.java
@@ -0,0 +1,30 @@
+public class Categorie {
+
+    // Attributes
+    private String nom;
+    private String description;
+
+    // Constructor
+    public Categorie(String nom, String description) {
+        this.nom = nom;
+        this.description = description;
+    }
+
+    // Getters
+    public String getNom() {
+        return nom;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    // Setters
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
