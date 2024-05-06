1a703d0bd2a9ff1e78d61e136a2624a38a9fc457
janis <janis.zaoui@grenoble-inp.org>
2024-05-06 09:43:34 +0200

    Classe Enchere

---
+++ auction-system/src/main/java/model/Enchere.java
@@ -0,0 +1,47 @@
+package model;
+
+public class Enchere {
+    private int ID_Enchere;
+    private int Email_utilisateur;
+    private float Prix_achat;
+    private Date Date;
+
+    public Enchere(int ID_Enchere, int email_utilisateur, float prix_achat, Date date) {
+        this.ID_Enchere = ID_Enchere;
+        this.Email_utilisateur = email_utilisateur;
+        this.Prix_achat = prix_achat;
+        this.Date = date;
+    }
+
+    public int getEmail_utilisateur() {
+        return Email_utilisateur;
+    }
+
+    public void setEmail_utilisateur(int email_utilisateur) {
+        Email_utilisateur = email_utilisateur;
+    }
+
+    public int getID_Enchere() {
+        return ID_Enchere;
+    }
+
+    public void setID_Enchere(int ID_Enchere) {
+        this.ID_Enchere = ID_Enchere;
+    }
+
+    public float getPrix_achat() {
+        return Prix_achat;
+    }
+
+    public void setPrix_achat(float prix_achat) {
+        Prix_achat = prix_achat;
+    }
+
+    public Date getDate() {
+        return Date;
+    }
+
+    public void setDate(Date date) {
+        this.Date = date;
+    }
+}
