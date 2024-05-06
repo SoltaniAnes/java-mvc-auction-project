5c351045264c7ff4f2b0f7f3d69a2427e495dab5
Lilian Steimer <lilian.steimer@grenoble-inp.org>
2024-05-06 09:29:38 +0200

    Added SalleDeVente class

---
+++ auction-system/src/main/java/model/SalleDeVente.java
@@ -0,0 +1,61 @@
+public class SalleDeVente {
+
+    // Attributes
+    private int id_salle_de_vente;
+    private Categorie categorie;
+    private String nom; // Optional ?
+    private String description;
+    private <ArrayList>Vente ventes;
+
+    // Constructor
+    public SalleDeVente(Categorie categorie, String nom, String description) {
+        this.categorie = categorie;
+        this.nom = nom;
+        this.description = description;
+    }
+
+    // Getters
+    public Categorie getCategorie() {
+        return categorie;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public <ArrayList>Vente getVentes() {
+        return ventes;
+    }
+
+    // Setters
+    public void setCategorie(Categorie categorie) {
+        this.categorie = categorie;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setVentes(<ArrayList>Vente ventes) {
+        this.ventes = ventes;
+    }
+
+    // Functions
+    // Add a vente to the list of ventes
+    public void addVente(Vente vente) {
+        ventes.add(vente);
+    }
+
+    // Remove a vente from the list of ventes
+    public void removeVente(Vente vente) {
+        ventes.remove(vente);
+    }
+}
