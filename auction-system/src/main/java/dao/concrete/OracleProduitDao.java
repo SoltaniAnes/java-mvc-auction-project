b1081bd54334d6ae3f03c18fafd32fa7aaba6b95
soltani <mohamed-anes.soltani@grenoble-inp.fr>
2024-05-13 11:56:08 +0200

    Adding Product DAO and test

---
+++ auction-system/src/main/java/dao/concrete/OracleProduitDao.java
@@ -0,0 +1,4 @@
+package main.java.dao.concrete;
+
+public class OracleProduitDao {
+}
---
+++ auction-system/src/main/java/dao/interfaces/ProduitDao.java
@@ -0,0 +1,13 @@
+package main.java.dao.interfaces;
+
+import java.sql.SQLException;
+import java.util.List;
+import main.java.model.Produit;
+
+public interface ProduitDao {
+    Produit insert(Produit produit) throws SQLException;
+    List<Produit> all() throws SQLException;
+    int deleteAll() throws SQLException;
+    int delete(Produit produit) throws SQLException;
+    Produit findById(UUID id) throws SQLException;
+}
